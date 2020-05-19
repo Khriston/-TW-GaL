b3c11c1392b7cf6f2f4390ba2b2e771982062012
Khriston <33064635+Khriston@users.noreply.github.com>
2020-05-19 08:12:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 var slideNo = 0;
-var slideTimeout = 4100;
+var slideTimeout = 6000;
 var slideshowImages = ["images/slide1.jpg",
 	"images/slide2.jpg",
 	"images/slide3.jpg"];
-var slideshowDescriptions = ["First image of the slideshow: 1",
-	"Second image of the slideshow: 2",
-	"Third image of the slideshow: 3"
-];
-var slideshowHeaders = ["Text1.",
-			"Text2.",
-			"Text3."];
+
+var slideshowHeaders = ["Fun",
+			"Smart",
+			"Intuitive"];
 
 	slideshow();
 
@@ -17,12 +14,8 @@ function slideshow() {
 	var slide = document.getElementById("slide");
 	slide.classList.add("fadeOut");
 	setTimeout(function() {
-		document.getElementById("slide").querySelector("img").src = 
-			slideshowImages[slideNo];
-		document.getElementById("slide").querySelector("img").alt =
-			slideshowDescriptions[slideNo];
-		document.getElementById("slide").querySelector("img").innerHTML = 
-			slideshowHeaders[slideNo];
+		document.getElementById("slide").querySelector("img").src = slideshowImages[slideNo];
+		document.getElementById("slide").querySelector("h2").innerHTML = slideshowHeaders[slideNo];
 		slide.classList.remove("fadeOut");
 	}, 1600);
 
