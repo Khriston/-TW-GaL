45c3b8b832ad94012c45e0461fd14ea5cb526f96
Khriston <33064635+Khriston@users.noreply.github.com>
2020-05-19 13:49:41 +0300

    Update Header.php

--- Header.php
+++ Header.php
@@ -3,7 +3,7 @@
 
 		public function __construct() {
 			echo "<header id=\"main-header\">";
-			echo "<a href=\"index.php\"><img src=\"images/logo.png\" style =\"width:20%; height:5%;\" /></a>";
+			echo "<a href=\"index.php\"><img src=\"images/logo.png\" style =\"width:50%; height:40%;\" /></a>";
 			echo "<h1>Backgammon</h1>";
 			echo "</header>";
 		}
