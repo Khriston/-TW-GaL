375e2027363a8b6fa4c70fdfd48b8f7275dafc79
Gradinariu Tudor <tudor.gradinariu@info.uaic.ro>
2020-06-10 17:22:20 +0300

    Export

--- ajax_export.php
+++ ajax_export.php
@@ -5,8 +5,10 @@
 		$query = "SELECT * FROM users where id='".$id."'";
 		$result = mysqli_query($db, $query);
 		$row = mysqli_fetch_array($result);
-
-		print '<h3>Backgammon score: ' . htmlentities($row[4], ENT_QUOTES) . '</h3>';
-		print '<h3>Tic-Tac-Toe score: ' . htmlentities($row[5], ENT_QUOTES) . '</h3>';
-		print '<h3>Draughts score: ' . htmlentities($row[6], ENT_QUOTES) . '</h3>';
+		print '<br>';
+		print '<textarea  rows="4" cols="70">';
+		print 'Backgammon score: ' . htmlentities($row[4], ENT_QUOTES). ', ' ;
+		print 'Tic-Tac-Toe score: ' . htmlentities($row[5], ENT_QUOTES). ', ' ;
+		print 'Draughts score: ' . htmlentities($row[6], ENT_QUOTES) ;
+		print '</textarea>';
 ?>
